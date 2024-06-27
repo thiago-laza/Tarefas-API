3b62ac250d6f362c41bca70676e19296659d7a02
thiago-laza <thiago.profmat@gmail.com>
2024-06-27 16:35:17 -0300

    Ajuste na classe Usuario(id)

--- tarefas-api/src/main/java/tarefas/api/model/GeneratedValue.java
+++
@@ -1,5 +0,0 @@
-package tarefas.api.model;
-
-public @interface GeneratedValue {
-
-}
--- tarefas-api/src/main/java/tarefas/api/model/Usuario.java
+++ tarefas-api/src/main/java/tarefas/api/model/Usuario.java
@@ -1,6 +1,8 @@
 package tarefas.api.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@GeneratedValue
+
 public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String nome;
     private String email;
 }
