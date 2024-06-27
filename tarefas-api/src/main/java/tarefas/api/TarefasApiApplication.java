40f43fe6d5bdfa18637ad2874f523564dc377081
thiago-laza <thiago.profmat@gmail.com>
2024-06-27 16:23:09 -0300

    Criacao dos arquivos do projeto

---
+++ tarefas-api/src/main/java/tarefas/api/TarefasApiApplication.java
@@ -0,0 +1,13 @@
+package tarefas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefasApiApplication.class, args);
+	}
+
+}
